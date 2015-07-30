4e809abee981eb77946339399108d1d1b1273b0b
dark.H <darkhackdevil@gmail.com>
2015-07-31 07:24:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+#remote-cmd  Document
+>Include Part
+ - remote command 
+ - remote vi
+ 
+ 
+## Introduction 
+
+	this is a script of putting remote as local system .
+
+
+## Install 
+## Useage 
---
+++ src/remote.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SAVE_TAG_PATH="$HOME/.tag_for_remote";
+
+
+PRE=$1;
+ARGV="${@:2}";
+REMOTE_URL="$(grep $PRE $SAVE_TAG_PATH | awk '{print $2}' |xargs )" ;
+
+if [[ -z $REMOTE_URL ]] 
+then
+    ARGV="$@";
+    $ARGV;
+    exit;
+fi
+
+echo "$ARGV";
+echo "$(ssh $REMOTE_URL $ARGV)";
