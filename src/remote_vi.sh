0d9d3b9c25c6622b558959d02e606b501379bbae
dark.H <darkhackdevil@gmail.com>
2015-08-27 12:24:51 +0800

    Stone-Gate

--- src/remote_vi.sh
+++ src/remote_vi.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 
-SAVE_TAG_PATH="$HOME/.tag_for_remote";
+SAVE_TAG_T_PATH="$HOME/.tag_for_remote";
 
 
 PRE=$1;
 ARGV="${@:2}";
-REMOTE_URL="$(grep $PRE $SAVE_TAG_PATH | awk '{print $2}' |xargs )" ;
+REMOTE_URL="$(grep $PRE $SAVE_TAG_T_PATH | awk '{print $2}' |xargs )" ;
 
+echo "Command : $ARGV";
 
-if [ $# < 4 ] ;
+if [[ $# < 3 ]] ;
 then
-	$@;
-	exit;
+    $@;
+    exit;
 else
 
-	
-	if [[ -z $REMOTE_URL ]]
-	then
-		ARGV="$@";
-		$ARGV;
-		exit;
-	fi
+    
+    if [[ -z $REMOTE_URL ]]
+    then
+	ARGV="$@";
+	echo "no remote url";
+	$ARGV;
+	exit;
+    fi
 
 
-	if [[ ${ARGV:0:1} == "vi" ]] ;
-	then
-		PATH=${ARGV:1:2};
-		scp "$REMOTE_URL:$PATH"  "./temp-$PATH" && vi  "./temp-$PATH" ;
-		scp "./temp-$PATH"  "$REMOTE_URL:$PATH";
-	fi
+    if [[ ${ARGV:0:2} == "vi" ]] ;
+    then
+	T_PATH=${ARGV:2};
+	scp  $REMOTE_URL:${T_PATH:1} ./temp-${T_PATH:4} && sleep 1 &&  vim  ./temp-${T_PATH:3} ;
+	scp ./temp-${T_PATH:3} $REMOTE_URL:${T_PATH:1}  && rm ./temp-${T_PATH:3} ;
+    
+    else
+	ssh  $REMOTE_URL $ARGV;
+    fi
 fi
 
